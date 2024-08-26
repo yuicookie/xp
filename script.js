e2077ce28118e5a7ec2244447858a6062f441914
yuicookie <104888283+yuicookie@users.noreply.github.com>
2024-08-27 00:13:34 +0900

    Add files via upload
    レベル100までの経験値計算に対応しました。

--- script.js
+++ script.js
@@ -8,28 +8,59 @@ function calc(Level, Rate, Xp) {
     NextXP = XP[NextXP];
     const messageNext = document.getElementById('message-next');
     const messageRun = document.getElementById('message-run');
+    const messageNext100 = document.getElementById('message-next100');
+    const messageRun100 = document.getElementById('message-run100');
     document.getElementById('level').style.border = '';
     if (Level === '') {
+        messageNext.textContent = '';
+        messageRun.textContent = '';
+        messageNext100.textContent = '';
+        messageRun100.textContent = '';
         document.getElementById('level').style.border = '2px solid red';
-        messageNext.textContent = '現在のレベルを入力してください。';
         messageNext.style.color = 'red';
+        messageNext.textContent = '現在のレベルを入力してください。';
         return;
     }
+    else if (Level == 0){
+        messageNext.textContent = '';
+        messageRun.textContent = '';
+        messageNext100.textContent = '';
+        messageRun100.textContent = '';
+        document.getElementById('level').style.border = '2px solid red';
+        messageNext.style.color = 'red';
+        messageNext.textContent = '現在のレベルに1以上を入力してください。';
+        return;
+    }
+
     if (Rate === '' || Rate == 0) {
+        Rate = 0;
     }
     else {
         RateXP = (Rate / 100) * NextXP;
         NextXP = NextXP - RateXP;
     }
+
+    //100レベルまでの経験値
+    SumXP = 0;
+    for (i = +Level + 1; i < 100; i++) {
+        TempXP = "Lv"+i;
+        SumXP += XP[TempXP];
+    }
+    SumXP += NextXP;
+
     if (Xp === '' || Xp == 0) {
         messageRun.textContent = '';
+        messageRun100.textContent = '';
     }
     else {
         NextRun = Math.ceil(NextXP / Xp);
         messageRun.innerHTML = 'あと <span style="color: red; font-weight: bold;">'+NextRun+'</span> 回走るとレベルアップ！';
+        NextRun100 = Math.ceil(SumXP / Xp);
+        messageRun100.innerHTML = 'あと <span style="color: red; font-weight: bold;">'+NextRun100+'</span> 回走るとレベルマックス！';    
     }
-    messageNext.innerHTML = '次のレベルまで <span style="color: red; font-weight: bold;">'+NextXP+'</span> 経験値！';
     messageNext.style.color = 'black';
+    messageNext.innerHTML = '次のレベルまで <span style="color: red; font-weight: bold;">'+NextXP+'</span> 経験値！';
+    messageNext100.innerHTML = 'レベル100まで <span style="color: red; font-weight: bold;">'+SumXP+'</span> 経験値！';
 }
 
 //Cookieに値を設定する関数
